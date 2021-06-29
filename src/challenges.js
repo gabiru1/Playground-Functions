12dd3bfbd58029de41a3ff05777328dffef93e8a
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-06-29 18:46:41 -0300

    requisito 6

--- src/challenges.js
+++ src/challenges.js
@@ -22,20 +22,28 @@ function splitSentence(str) {
 // Desafio 4
 function concatName(array) {
   // seu código aqui
-  let stringComVirgula = array[array.length - 1] + ", " + array[0];
+  let stringComVirgula = array[array.length - 1] + ', ' + array[0];
   return stringComVirgula;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
-  let pontos = (wins * 3) + ties
-  return pontos
+  let pontos = wins * 3 + ties;
+  return pontos;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(array) {
   // seu código aqui
+  let maxNumber = Math.max(...array);
+  let quantidade = 0;
+  for (const number of array) {
+    if (number === maxNumber) {
+      quantidade += 1;
+    }
+  }
+  return quantidade;
 }
 
 // Desafio 7
