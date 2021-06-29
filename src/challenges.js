51826ea6e2fabe23b3e1431009c648d8be7b587a
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-06-29 18:15:04 -0300

    requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -27,8 +27,10 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let pontos = (wins * 3) + ties
+  return pontos
 }
 
 // Desafio 6
